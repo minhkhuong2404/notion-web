29c89254a0a157993736974ffbf8d120b4fe7094
minhkhuong2404 <13156@student.vgu.edu.vn>
2023-05-01 23:49:57 +0700

    default theme should be dark

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import {ThemeProvider} from 'next-themes'
 import { GoogleAnalytics } from "nextjs-google-analytics";
+import { Analytics } from'@vercel/analytics/react';
 import '@/styles/globals.css';
 import "react-medium-image-zoom/dist/styles.css";
 import 'prismjs/themes/prism-tomorrow.css';
@@ -11,7 +12,8 @@ import type { AppProps } from 'next/app'
 export default function App({ Component, pageProps }: AppProps) {
   return <>
     <GoogleAnalytics trackPageViews />
-    <ThemeProvider attribute="class" defaultTheme={"light"}>
+		<Analytics />
+    <ThemeProvider attribute="class" defaultTheme={"dark"}>
       <Component {...pageProps} />
     </ThemeProvider>
   </>
